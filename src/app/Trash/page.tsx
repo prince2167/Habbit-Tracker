bd924bd87c443e236d56fc10994e62a09a38259e
Prince Singh <prince.dev205@gmail.com>
2023-09-09 22:54:31 +0530

    create page

--- src/app/Trash/page.tsx
+++ src/app/Trash/page.tsx
@@ -1,10 +1,21 @@
-import { Layout } from '../../components/index'
-import React from 'react'
+"use client";
+
+import { useAppSelector } from "@/store";
+import { HabitCard, Layout } from "../../components/index";
 
 function Trash() {
+  const trash = useAppSelector((state) => state.habit.trash);
   return (
-    <Layout>Trash</Layout>
-  )
+    <Layout>
+      <h1 className="text-xl font-semibold">Trash</h1>
+      {trash.length === 0 && <h1 className="text-2xl font-semibold text-center">Trash is empty!</h1>}
+      <div className=" flex flex-wrap  gap-24">
+        {trash?.map((habit) => (
+          <HabitCard habit={habit} key={habit.title} />
+        ))}
+      </div>
+    </Layout>
+  );
 }
 
-export default Trash
+export default Trash;

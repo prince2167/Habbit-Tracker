840f6e573c817a582d521cd6413d5b3537edcebd
Prince Singh <prince.dev205@gmail.com>
2023-09-09 22:56:02 +0530

    create archive page

--- src/app/archive/page.tsx
+++ src/app/archive/page.tsx
@@ -1,9 +1,22 @@
-import { Layout } from '../../components/index'
+"use client";
 
-function Archive() {
+import { useAppSelector } from "@/store";
+import { HabitCard, Layout } from "../../components/index";
+
+function Trash() {
+  const archive = useAppSelector((state) => state.habit.archive);
+  
   return (
-    <Layout>Archive</Layout>
-  )
+    <Layout>
+      <h1 className="text-xl font-semibold">archive</h1>
+      {archive.length === 0 && <h1 className="text-2xl font-semibold text-center">Archive is empty!</h1>}
+      <div className=" flex flex-wrap  gap-24">
+        {archive?.map((habit) => (
+          <HabitCard habit={habit} key={habit.title} />
+        ))}
+      </div>
+    </Layout>
+  );
 }
 
-export default Archive
+export default Trash;

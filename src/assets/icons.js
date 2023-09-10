0cca29b87f1860ba0a2d55e1b04acf945ec01323
Prince Singh <prince.dev205@gmail.com>
2023-09-10 18:12:24 +0530

    icons

---
+++ src/assets/icons.js
@@ -0,0 +1,19 @@
+import { GoHome } from "react-icons/go";
+import { MdLabelOutline, MdArchive, MdUnarchive } from "react-icons/md";
+import { BsArchive } from "react-icons/bs";
+import { FaRegTrashAlt, FaTrash, FaTrashRestore } from "react-icons/fa";
+import { CgProfile } from "react-icons/cg";
+import { BiSolidEdit } from "react-icons/bi";
+
+export {
+  GoHome,
+  MdLabelOutline,
+  BsArchive,
+  FaRegTrashAlt,
+  CgProfile,
+  BiSolidEdit,
+  FaTrash,
+  FaTrashRestore,
+  MdArchive,
+  MdUnarchive,
+};

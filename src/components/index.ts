0538e4d1b8ae0ea5ac7c41640c7271131296ce5a
Prince Singh <prince.dev205@gmail.com>
2023-09-14 12:07:28 +0530

    import modal for edit habit

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,7 @@ import Navbar from "./Navbar/Navbar";
 import Sidebar from "./Sidebar/Sidebar";
 import Layout from "./Layout/Layout";
 import CreateHabitModal from "./Modal/CreateHabitModal";
+import EditForm from "./Modal/EditForm";
 import ThemeToggle from "./ThemeToggle.tsx/ThemeToggle";
 import HabitCard from "./HabitCard/HabitCard";
-export {Navbar,Sidebar,Layout,CreateHabitModal,ThemeToggle,HabitCard}
+export { Navbar, Sidebar, Layout, CreateHabitModal, ThemeToggle, HabitCard, EditForm }

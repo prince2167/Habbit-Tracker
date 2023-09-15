966deafc5b589ca3d556f5a8ef745bc9579428a8
Prince Singh <prince.dev205@gmail.com>
2023-09-15 10:25:22 +0530

    feat logout

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,8 +1,32 @@
-import { Layout } from '../../components/index'
+"use client";
+
+import { Layout } from "../../components/index";
+import axios from "axios";
+import Link from "next/link";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+
 function Profile() {
+  const router = useRouter();
+  const logout = async () => {
+    try {
+      await axios.get("/api/auth/logout");
+      toast.success("Logout successful");
+      router.push("/signin");
+    } catch (error: any) {
+      toast.error(error.message);
+    }
+  };
   return (
-    <Layout>Profile</Layout>
-  )
+    <Layout>
+      <button
+        onClick={logout}
+        className="bg-blue-500 text-white font-bold py-2 px-4 rounded"
+      >
+        Logout
+      </button>
+    </Layout>
+  );
 }
 
-export default Profile
+export default Profile;

ea35256344ddbef2aad3942dc4951f5a1d5a6996
Prince Singh <prince.dev205@gmail.com>
2023-09-15 10:29:19 +0530

    feat: signup & login

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,20 +1,46 @@
 "use client";
 
-import { useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { Loader } from "@/components";
+import toast from "react-hot-toast";
+import axios from "axios";
 
 const Signin = () => {
-  const [authState, setAuthState] = useState({
+  const router = useRouter();
+  const [isButtonDisabled, setIsbuttonDisabled] = useState(true);
+  const [isLoading, setIsloading] = useState(false);
+  const [user, setUser] = useState({
     email: "",
     password: "",
   });
 
-  const formSubmit = () => {
-    console.log(authState, "auth");
+  const formSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try {
+      setIsloading(true);
+      const response = await axios.post("/api/auth/signin", user);
+      console.log("Login Successful", response);
+      toast.success("Login Successful");
+      router.push("/");
+    } catch (error: any) {
+      toast.error(error.message)
+    } finally {
+      setIsloading(false);
+    }
   };
+
+  useEffect(() => {
+    if (user.email.length > 0 && user.password.length > 0) {
+      setIsbuttonDisabled(false);
+    } else {
+      setIsbuttonDisabled(true);
+    }
+  }, [user]);
   return (
     <section>
-      <div className="flex items-center justify-center px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24 ">
+      <div className=" relative flex items-center justify-center px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24 ">
         <div className="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md dark:bg-[#111014]">
           <div className="mb-2 flex justify-center">
             <img
@@ -47,9 +73,9 @@ const Signin = () => {
                     className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
                     type="email"
                     placeholder="Email"
-                    value={authState.email}
+                    value={user.email}
                     onChange={(event) =>
-                      setAuthState({ ...authState, email: event.target.value })
+                      setUser({ ...user, email: event.target.value })
                     }
                   ></input>
                 </div>
@@ -73,9 +99,9 @@ const Signin = () => {
                     className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
                     type="password"
                     placeholder="Password"
-                    value={authState.password}
+                    value={user.password}
                     onChange={(event) =>
-                      setAuthState({ ...authState, password: event.target.value })
+                      setUser({ ...user, password: event.target.value })
                     }
                   ></input>
                 </div>
@@ -84,7 +110,9 @@ const Signin = () => {
               <div>
                 <button
                   type="submit"
-                  className="inline-flex w-full items-center justify-center rounded-md bg-black dark:bg-white dark:text-black px-3.5 py-2.5 font-semibold leading-7 text-white hover:bg-black/80 "
+                  className={`inline-flex w-full items-center justify-center rounded-md bg-black dark:bg-white dark:text-black px-3.5 py-2.5 font-semibold leading-7 text-white hover:bg-black/80 ${
+                    isButtonDisabled ? " opacity-60 cursor-not-allowed" : ""
+                  }`}
                 >
                   Get started
                 </button>
@@ -111,6 +139,30 @@ const Signin = () => {
           </div>
         </div>
       </div>
+      {isLoading && (
+        <div
+          role="status"
+          className="absolute -translate-x-1/2 -translate-y-1/2 top-2/4 left-1/2"
+        >
+          <svg
+            aria-hidden="true"
+            className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+            viewBox="0 0 100 101"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+              fill="currentColor"
+            />
+            <path
+              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+              fill="currentFill"
+            />
+          </svg>
+          <span className="sr-only">Loading...</span>
+        </div>
+      )}
     </section>
   );
 };
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,20 +1,53 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect, FormEvent } from "react";
 import Link from "next/link";
+import { useRouter } from 'next/navigation'
+import { Loader } from "@/components";
+import toast from "react-hot-toast";
+import axios from "axios";
 
 const Singup = () => {
-  const [authState, setAuthState] = useState({
+  const router = useRouter()
+  const [isButtonDisabled, setIsbuttonDisabled] = useState(true);
+  const [isLoading, setIsloading] = useState(false);
+  const [user, setUser] = useState({
     name: "",
     email: "",
     password: "",
     confirmPassword: "",
   });
-  console.log(authState, "auth");
-  const formSubmit = () => {};
+
+  useEffect(() => {
+    if (
+      user.name.length > 0 &&
+      user.email.length > 0 &&
+      user.password.length > 0 &&
+      user.confirmPassword.length > 0
+    ) {
+      setIsbuttonDisabled(false);
+    } else {
+      setIsbuttonDisabled(true);
+    }
+  }, [user]);
+
+  const formSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try {
+      setIsloading(true);
+      const response = await axios.post("/api/auth/signup", user);
+      console.log(response.data, "reponse");
+      router.push("/signin")
+    } catch (error: any) {
+      console.log("signedup failed", error.message);
+      toast.error(error.message);
+    } finally {
+      setIsloading(false);
+    }
+  };
   return (
     <section>
-      <div className="flex items-center justify-center px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24">
+      <div className=" relative flex items-center justify-center px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24">
         <div className="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md">
           <div className="mb-2 flex justify-center">
             <img
@@ -49,9 +82,9 @@ const Singup = () => {
                     type="text"
                     placeholder="Full Name"
                     id="name"
-                    value={authState.name}
+                    value={user.name}
                     onChange={(event) =>
-                      setAuthState({ ...authState, name: event.target.value })
+                      setUser({ ...user, name: event.target.value })
                     }
                   ></input>
                 </div>
@@ -68,9 +101,9 @@ const Singup = () => {
                     type="email"
                     placeholder="Email"
                     id="email"
-                    value={authState.email}
+                    value={user.email}
                     onChange={(event) =>
-                      setAuthState({ ...authState, email: event.target.value })
+                      setUser({ ...user, email: event.target.value })
                     }
                   ></input>
                 </div>
@@ -89,10 +122,10 @@ const Singup = () => {
                     type="password"
                     placeholder="Password"
                     id="password"
-                    value={authState.password}
+                    value={user.password}
                     onChange={(event) =>
-                      setAuthState({
-                        ...authState,
+                      setUser({
+                        ...user,
                         password: event.target.value,
                       })
                     }
@@ -116,10 +149,10 @@ const Singup = () => {
                     type="password"
                     placeholder=" Confirm Password"
                     id="confirm-password"
-                    value={authState.confirmPassword}
+                    value={user.confirmPassword}
                     onChange={(event) =>
-                      setAuthState({
-                        ...authState,
+                      setUser({
+                        ...user,
                         confirmPassword: event.target.value,
                       })
                     }
@@ -130,7 +163,9 @@ const Singup = () => {
               <div>
                 <button
                   type="submit"
-                  className="inline-flex w-full items-center justify-center rounded-md bg-black dark:bg-white dark:text-black px-3.5 py-2.5 font-semibold leading-7 text-white hover:bg-black/80"
+                  className={`inline-flex w-full items-center justify-center rounded-md bg-black dark:bg-white dark:text-black px-3.5 py-2.5 font-semibold leading-7 text-white hover:bg-black/80 ${
+                    isButtonDisabled ? " opacity-60 cursor-not-allowed" : ""
+                  }`}
                 >
                   Create Account
                 </button>
@@ -158,6 +193,31 @@ const Singup = () => {
           </div>
         </div>
       </div>
+
+      {isLoading && (
+        <div
+          role="status"
+          className="absolute -translate-x-1/2 -translate-y-1/2 top-2/4 left-1/2"
+        >
+          <svg
+            aria-hidden="true"
+            className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+            viewBox="0 0 100 101"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+              fill="currentColor"
+            />
+            <path
+              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+              fill="currentFill"
+            />
+          </svg>
+          <span className="sr-only">Loading...</span>
+        </div>
+      )}
     </section>
   );
 };

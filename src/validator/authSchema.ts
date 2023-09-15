f632931da9fff7e294c7328dd6bbc6bd112f8fea
Prince Singh <prince.dev205@gmail.com>
2023-09-15 10:24:48 +0530

    validator

--- src/validator/authSchema.ts
+++ src/validator/authSchema.ts
@@ -4,4 +4,6 @@ export const signupSchema = vine.object({
     name: vine.string().trim().minLength(2).maxLength(30),
     email: vine.string().email(),
     password: vine.string().minLength(6).maxLength(20).confirmed(),
+
+
 });
